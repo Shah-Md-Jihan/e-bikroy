0c8a613dd09f41a1daff93a11c146bc1cf440b4d
Shah Md. Jihan <shahmohammadjihan@gmail.com>
2022-11-24 13:01:41 +0600

    main layout component added

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,18 +1,12 @@
 import React from "react";
-import Footer from "../../Shared/Footer/Footer";
-import Navbar from "../../Shared/Navbar/Navbar";
 import SearchField from "../../Shared/SearchField/SearchField";
 import Category from "./Category";
 
 const Home = () => {
   return (
     <div>
-      <header>
-        <Navbar></Navbar>
-      </header>
       <SearchField></SearchField>
       <Category></Category>
-      <Footer></Footer>
     </div>
   );
 };
